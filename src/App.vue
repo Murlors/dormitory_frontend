1a6fc1b398d83283bbbcee8780ca273b810e53c8
Murlors <1595258509@qq.com>
2024-09-10 10:07:14 +0800

    ✨: 搭建vue基础框架

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-title class="app" title="宿舍管理系统"></div>
+  <div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+
+export default {
+  name: "Layout",
+}
+</script>
